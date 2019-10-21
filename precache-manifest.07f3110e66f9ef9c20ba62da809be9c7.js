3f1fb4cd8eca520dee666080ef6a1fb3a570af35
iambia <bia.paulaa@gmail.com>
2019-10-21 20:31:45 +0200

    Updates

---
+++ precache-manifest.07f3110e66f9ef9c20ba62da809be9c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab27304d0de639294d2efafd2282c45f",
+    "url": "/Social-Card/index.html"
+  },
+  {
+    "revision": "562ff26448a68b765b45",
+    "url": "/Social-Card/static/css/main.18ad2b42.chunk.css"
+  },
+  {
+    "revision": "3ea234ae08fd1c6cf719",
+    "url": "/Social-Card/static/js/2.38ab6cfa.chunk.js"
+  },
+  {
+    "revision": "562ff26448a68b765b45",
+    "url": "/Social-Card/static/js/main.aa76d8a0.chunk.js"
+  },
+  {
+    "revision": "c2b3f81433d17c3fe48f",
+    "url": "/Social-Card/static/js/runtime-main.f7c19f14.js"
+  }
+]);
